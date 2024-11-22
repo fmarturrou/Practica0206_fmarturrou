445d9d2e5adff56a3a9b7eaafeddb2c49deb6f7b
fmarturrou <fmarturrou1@educacion.navarra.es>
2024-11-22 09:47:35 +0100

    Ejercicios

--- Ejercicio03.py
+++ Ejercicio03.py
@@ -3,6 +3,6 @@ artículo = input("Escribe un artículo de supermercado\n")
 unidades = int(input("Pon cuantas unidades quieres de ese artículo\n"))
 if artículo in diccionario:
     precio = diccionario[artículo] * unidades
-    print(f"El precio de {unidades} unidades de {artículo} es: {precio}€")
+    print(f"El precio de {unidades} unidades de {artículo} es de {precio}€")
 else:
     print(f"El artículo {artículo} no está disponible en el diccionario.")
